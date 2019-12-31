5c307995dd1a2e96e3e3f580176f6a14721906c8
Justin <twoplatinumchainz@gmail.com>
2019-12-31 01:26:43 -0800

    replace scheduled events with new ones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var now = moment();
 var current = moment().format('dddd, MMMM Do YYYY');
-var schedule = [];
+var scheduleArr = [];
+var storedSchedule;
 
 $('#title-date').html('Event schedule for <b>' + current + '</b>');
 
@@ -8,10 +9,33 @@ $(document).ready(function() {
   init();
 
   function init() {
-    saveEvent();
+    // saveEventV02();
+    saveEventV03();
   }
 
-  function saveEvent() {
+  //   function saveEventV02() {
+  //     $('.save-button').on('click', function() {
+  //       var trId = $(this)
+  //         .closest('tr')
+  //         .attr('id');
+  //       var textAreaVal = $(this)
+  //         .closest('tr')
+  //         .find('textarea')
+  //         .val();
+  //       var scheduleObj = {};
+
+  //       if (textAreaVal === '') {
+  //         return;
+  //       } else {
+  //         checkExisting(trId);
+  //         scheduleObj[trId] = textAreaVal.trim();
+  //         scheduleArr.push(scheduleObj);
+  //         localStorage.setItem('schedule', JSON.stringify(scheduleArr));
+  //       }
+  //     });
+  //   }
+
+  function saveEventV03() {
     $('.save-button').on('click', function() {
       var trId = $(this)
         .closest('tr')
@@ -19,18 +43,66 @@ $(document).ready(function() {
       var textAreaVal = $(this)
         .closest('tr')
         .find('textarea')
-        .val();
+        .val()
+        .trim();
       var scheduleObj = {};
+      storedSchedule = JSON.parse(localStorage.getItem('schedule'));
 
-      if (textAreaVal === '') {
-        return;
+      //   if (storedSchedule !== null) {
+      //     for (var i = 0; i < storedSchedule.length; i++) {
+      //       if (storedSchedule[i].hasOwnProperty(trId)) {
+      //         storedSchedule[i][trId] = textAreaVal;
+      //         console.log('stuck here');
+      //       } else {
+      //         scheduleObj[trId] = textAreaVal;
+      //         scheduleArr.push(scheduleObj);
+      //         localStorage.setItem('schedule', JSON.stringify(scheduleArr));
+      //         console.log('adding new stuff to the array now');
+      //       }
+      //     }
+      //   } else {
+      //     scheduleObj[trId] = textAreaVal;
+      //     scheduleArr.push(scheduleObj);
+      //     localStorage.setItem('schedule', JSON.stringify(scheduleArr));
+      //     console.log('creating initial array');
+      //   }
+
+      if (storedSchedule === null) {
+        scheduleObj[trId] = textAreaVal;
+        scheduleArr.push(scheduleObj);
+        localStorage.setItem('schedule', JSON.stringify(scheduleArr));
+        console.log('creating initial array');
       } else {
-        scheduleObj[trId] = textAreaVal.trim();
-        schedule.push(scheduleObj);
+        for (var i = 0; i < storedSchedule.length; i++) {
+          if (storedSchedule[i].hasOwnProperty(trId)) {
+            storedSchedule[i][trId] = textAreaVal;
+            scheduleArr = storedSchedule;
+            localStorage.clear();
+            localStorage.setItem('schedule', JSON.stringify(scheduleArr));
+            console.log('replacing stuff');
+            return;
+          }
+        }
+        scheduleObj[trId] = textAreaVal;
+        scheduleArr.push(scheduleObj);
+        localStorage.setItem('schedule', JSON.stringify(scheduleArr));
+        console.log('adding new stuff to the array now');
       }
-
-      console.log(scheduleObj);
-      console.log(schedule);
     });
   }
+
+  //   function checkExisting(x) {
+  //     storedSchedule = JSON.parse(localStorage.getItem('schedule'));
+  //     console.log(storedSchedule);
+
+  //     if (storedSchedule !== null) {
+  //       for (var i = 0; i < storedSchedule.length; i++) {
+  //         if (storedSchedule[i].hasOwnProperty(x)) {
+  //           storedSchedule[i][x] =
+  //         } else {
+  //           console.log(storedSchedule.hasOwnProperty(x));
+  //           alert('no');
+  //         }
+  //       }
+  //     }
 });
